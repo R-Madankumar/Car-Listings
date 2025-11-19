d3c411c972bae55ad5a2d1842d22b5d5a7fd39d4
Your Name <your.email@example.com>
2025-11-19 16:09:27 +0530

    fixed mongo session

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const path = require("path");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
 const session = require("express-session");
-const MongoStore =require("connect-mongo");
+const MongoStore = require('connect-mongo');
 const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStratergy = require("passport-local");
@@ -41,33 +41,22 @@ app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname,"/public")));
 
-// mongo session store
-const store =MongoStore.create({
-    mongoUrl : dbUrl,
-    crypto:{
-        secret:process.env.SECRET,
-    },
-    touchAfter:24*3600,
-})
 
-store.on("error",()=>{
-    console.log("Error in Monogo Session Store",err);
-})
 
 // session setup
-const sessionOptions = {
-    store,
-    secret : process.env.SECRET,
-    resave : false,
-    saveUninitialized : true,
-    cookie:{
-        expires:Date.now()+7*24*60*60*1000,
-        maxAge : 7*24*60*60*1000,
+const sessionOption = {
+    store: MongoStore.create({ mongoUrl: dbUrl }),
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: 7 * 24 * 60 * 60 * 1000,
         httpOnly: true,
     }
 };
 
-app.use(session(sessionOptions));
+app.use(session(sessionOption));
 app.use(flash());
 
 app.use(passport.initialize());
@@ -98,9 +87,11 @@ app.all("*",(req,res,next)=>{
     next(new ExpressError(404,"Page not found"));
 })
 
-app.use((error,req,res,next)=>{
-    let {statusCode = 500, message = "something went wrong"} = error;
-    res.status(statusCode).render("error",{error});
+app.use((error, req, res, next) => {
+    if (res.headersSent) return next(error);
+    let { statusCode = 500 } = error;
+    if (!error.message) error.message = "Something went wrong";
+    return res.status(statusCode).render("error.ejs", { error });
 })
 
 app.listen(8080,()=>{
